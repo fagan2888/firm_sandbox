c7795d946d71c52b4676ff1555bb19560dd883be
Rex <drexmcarthur@gmail.com>
2015-08-06 10:35:20 -0600

    Worked on House hold problem

--- Rex_OLG/SS_v1.py
+++ Rex_OLG/SS_v1.py
@@ -32,9 +32,9 @@ cbar2       = min of product 2 in composite good
 chi_b       = utility weight warm glow
 chi_n       = utility weight disutility of labor
 '''
-S = 10. #periods
-J = 4.  #Ability groups
-T = 25. #Time to Steady State  
+S = 10 #periods
+J = 4  #Ability groups
+T = 25 #Time to Steady State  
 A = 1.0 # Total Factor of Productivity 
 bin_weights = [.25, .25, .25, .25] #Weights for ability gropus
 gamma = .5 #Capital share of output  
@@ -44,14 +44,11 @@ alpha = .2  #Preference parameter for two goods (1-alpha) is other
 sigma = 1.9 #relative risk averstion for hh
 beta = .98  #Discount factor 
 max_l = 1.0 #max hours 
-cbar1 = 0.1 #min of product 1 
-cbar2 = 0.1 #min of product 2   
+cbar1 = 0. #min of product 1 
+cbar2 = 0. #min of product 2   
 chi_b = 0.2 #utility weight for bequest 
 chi_n = 0.5 #disutility weight for labor
 
-#Make an intial guess for r and w
-r = .2
-w = 2.0
 
 #Logical checks
 if np.sum(bin_weights) != 1.:
@@ -77,9 +74,10 @@ def get_p_tilde(p1, p2):
     p_tilde = ((p1/alpha)**alpha)*((p2/(1-alpha))**(1-alpha))
     return p_tilde
 
-def consump(w, r, n, k0, k, b2, p1, p2, p_tilde, j):
 
-    c = (((1+r)*k0) + w*n*e[j] - k + bq - (p1*cbar1) - (p2*cbar2)/p_tilde)
+def consump(w, r, n, k0, k, p1, p2, p_tilde, j):
+
+    c = (((1+r)*k0) + w*n*e[j] - k - (p1*cbar1) - (p2*cbar2)/p_tilde)
     return c
 
 def MUc(c):
@@ -94,13 +92,6 @@ def MUl(n):
     Marginal (dis)utility of labor
     '''
     mu_l = -chi_n*((ltilde-n)**(-nu))
-    return mu_l
-
-def MUb(bq):
-    '''
-    Marginal utility of Bequest
-    '''
-    mu_l = chi_b*(bq**(-nu))
     return mu_l
 
 def k_error(r,c,j):
@@ -127,21 +118,32 @@ def l_error(w, L_guess, c, p_tilde, j):
     lerror = (w*MUc(c)*e[j])/p_tilde + MUl(L_guess)
     return lerror
 
-def bq_error(K_guess, c, p_tilde):
-    '''
-    '''
-    berror = (MUc(c[-1,:]))/p_tilde - MUb(K_guess[-1,:])
-    return berror
-
 def solve_house(guessvec, r, w, p1, p2, p_tilde, j):
     '''
     '''
     k = guessvec[0:S]
     n = guesses[S:]
-    bq = (1+r)*(k)
+    k0 = np.zeros((S,1))
+    k0[1:,0] = k[:-1,0]
+    c = consump(w,r,n,k0,k,p1,p2,p_tilde,j)
+    kerror = k_error(r,c,j)
+    lerror = l_error(w,L_guess,c,p_tilde,j)
+
+    #Duct Tape
+    mask1 = n < 0
+    mask2 = n > ltilde
+    mask3 = k < 0
+    mask4 = c <= 0
+    lerror[mask1] += 1e10
+    lerror[mask2] += 1e10
+    kerror[mask3[:-1,0]] += 1e10
+    kerror[mask4[:-1,0]] += 1e10
+    return list(kerror)+list(lerror)
 
 
-
+#Make an intial guess for r and w
+r = .2
+w = 1.3
 
 p1 = get_p(r,w)
 p2 = get_p(r,w)
@@ -150,5 +152,7 @@ p_tilde = get_p_tilde(p1,p2)
 print p_tilde
 
 
+for j in xrange(J):
+    x = optimize.fsolve(solve_house, guessvec, args =(r, w, p1, p2, p_tilde, j))
 
 
